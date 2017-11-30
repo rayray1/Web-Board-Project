b7b8f06ad74c239b1ee03e709bff489a8a7e9b73
RayRay <RayRay>
2017-11-30 09:35:36 +0000

    folder edits

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-
+LOGIN_URL = 'login'
 LOGOUT_REDIRECT = 'home'
 LOGIN_REDIRECT = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
